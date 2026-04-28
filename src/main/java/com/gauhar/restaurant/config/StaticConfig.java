43bfe98e12d8ec7399d37d167a2673abcde33f9c
gauharsaduakas <gauharsaduakas05@gmail.com>
2026-04-29 02:41:40 +0500

    сро

--- src/main/java/com/gauhar/restaurant/config/StaticConfig.java
+++
@@ -1,8 +0,0 @@
-package com.gauhar.restaurant.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class StaticConfig implements WebMvcConfigurer {
-}
